e8ce8ec884d8525d4442a1a113bc65836d110057
EugeneBo <deafmuteman@gmail.com>
2018-08-22 18:27:17 +0300

    presenter.attachView() problem resolved

--- app/src/main/java/com/github/parfenovvs/mvpsample/base/BaseActivity.java
+++ app/src/main/java/com/github/parfenovvs/mvpsample/base/BaseActivity.java
@@ -5,45 +5,52 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseActivity<V extends BaseView, P extends BasePresenter<V>> extends AppCompatActivity {
-  private P presenter;
+    private P presenter;
 
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    attachPresenter();
-  }
-
-  @SuppressWarnings("unchecked")
-  private void attachPresenter() {
-    //Fragment f = new Fragment();
-    //f.setRetainInstance(true);
-    //f.setObject(onRetainCustomNonConfigurationInstance());
-    //getSupportFragmentManager().beginTransaction().add(f, "TAG").commit();
-    //
-    //Fragment savedFragment = getSupportFragmentManager().findFragmentByTag("TAG");
-    //savedFragment.getObject();
-    P presenter = (P) getLastCustomNonConfigurationInstance();
-    if (presenter == null) {
-      presenter = providePresenter();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        attachPresenter();
     }
-    presenter.attachView((V) this);
-    this.presenter = presenter;
-  }
 
-  @Override
-  protected void onDestroy() {
-    presenter.detachView();
-    super.onDestroy();
-  }
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void onStart() {
+        super.onStart();
+        presenter.attachView((V) this);
+    }
 
-  @Override
-  public Object onRetainCustomNonConfigurationInstance() {
-    return presenter;
-  }
+    @SuppressWarnings("unchecked")
+    private void attachPresenter() {
+        //Fragment f = new Fragment();
+        //f.setRetainInstance(true);
+        //f.setObject(onRetainCustomNonConfigurationInstance());
+        //getSupportFragmentManager().beginTransaction().add(f, "TAG").commit();
+        //
+        //Fragment savedFragment = getSupportFragmentManager().findFragmentByTag("TAG");
+        //savedFragment.getObject();
 
-  protected abstract P providePresenter();
+        P presenter = (P) getLastCustomNonConfigurationInstance();
+        if (presenter == null) {
+            presenter = providePresenter();
+        }
+        this.presenter = presenter;
+    }
 
-  protected final P getPresenter() {
-    return presenter;
-  }
+    @Override
+    protected void onDestroy() {
+        presenter.detachView();
+        super.onDestroy();
+    }
+
+    @Override
+    public Object onRetainCustomNonConfigurationInstance() {
+        return presenter;
+    }
+
+    protected abstract P providePresenter();
+
+    protected final P getPresenter() {
+        return presenter;
+    }
 }
